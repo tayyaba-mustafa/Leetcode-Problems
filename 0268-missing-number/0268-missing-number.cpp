aa0d2724d32f3fae45a8a838957897e09449d352
tayyaba-mustafa <tayyabamustafa1078@gmail.com>
2024-08-31 11:35:49 +0500

    Time: 16 ms (37.46%), Space: 20.7 MB (35.72%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+    int n=nums.size();
+    for (int i = 0; i < nums.size() ; i++) {
+            n += i - nums[i];
+        }
+
+        return n;
+    }
+};
